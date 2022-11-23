2cb63895a4350c425e394b5b8becebd75abda135
Thomas Vallentin <vallentin.thomas01@gmail.com>
2022-11-23 21:41:22 +0100

    Add todo

--- src/Scene/ScriptEngine.h
+++ src/Scene/ScriptEngine.h
@@ -18,6 +18,7 @@ public:
     static ScriptEngine& Init();
     inline static ScriptEngine& Get() { return s_instance; }
 
+    // TODO: Prevent Scripts that do not belong to the active scene from beeing added to the Engine
     void Register(Components::Script* script);
     void Deregister(Components::Script* script);
 

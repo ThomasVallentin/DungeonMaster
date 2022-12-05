3e5f2bb400535b9290104e433beec3d8b204ba25
Thomas Vallentin <vallentin.thomas01@gmail.com>
2022-12-05 14:41:18 +0100

    Add new GetDataBlock() method

--- src/Game/Components.h
+++ src/Game/Components.h
@@ -3,12 +3,29 @@
 
 #include "Scene/Components/Basics.h"
 
-#include "Navigation/Agent.h"
+#include "Navigation/Engine.h"
 
 
 namespace Components {
 
+struct CharacterControllerData
+{
+    Animation<glm::mat4> moveAnimation;
+    Animation<glm::mat4> attackAnimation;
+    float speed = 2.0f;
+    Navigation::CellFilters navFilter = Navigation::CellFilters::Default;
+};
+
 Scriptable CreateCharacterController(const Entity& entity);
+
+struct MonsterLogicData
+{
+    float speed = 2.0f;
+    float angleOfView = 45.0f;
+    float viewDistance = 4.0f;
+    Navigation::CellFilters navFilter = Navigation::CellFilters::Default;
+    Entity target;
+};
 
 Scriptable CreateMonsterLogic(const Entity& entity);
 
